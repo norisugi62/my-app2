adbca670db91dd804a1d5401c75de041fe63c46a
norisugi62 <noriaki197962@gmail.com>
2026-04-02 23:33:44 +0900

    修正

--- src/js/common/main.js
+++ src/js/common/main.js
@@ -2,11 +2,4 @@
 // import '/src/scss/style.scss';
 import '@/scss/style.scss';
 
-document.querySelector('#app').innerHTML = `<h1>Hello World!!!!!!</h1>`;
-
-const vall = 'のりあき';
-console.log('vall:', vall);
-
-var a = 1;
-
-console.log('a:', a);
+// document.querySelector('#app').innerHTML = `<h1>Hello World!!!!!!</h1>`;
